17a520fab1caa428acbff3aaa5d7149cc16a43c1
Texmo100 <isaac.ir@hotmail.com>
2021-06-16 15:19:41 -0700

    y-Calculator alpha version 1.0

--- src/components/Ycalculator.js
+++ src/components/Ycalculator.js
@@ -1,34 +1,128 @@
-import React from 'react'
+import React, { useState } from 'react'
+import NumPad from '../data/NumPad'
 
 const Ycalculator = () => {
+    const [result, setResult] = useState("0")
+    const [number, setNumber] = useState("0")
+
+    const numPadRender = NumPad.map(item => <button key={item.id} className={item.cssClass} onClick={() => buttonHandle(item)}>{item.value}</button>)
+
+    const buttonHandle = (item) => {
+        const { type, value } = item
+
+        if ((type === "operator") && (value === "C")) {
+            setResult("0")
+            setNumber("0")
+        }else if ((type === "operator") && (value === "=")){
+            if(centinel(result) === false){
+                setResult(result)
+            }else{
+                if(reaper(result).operator === "/"){
+                    let number1 = parseInt(reaper(result).number)
+                    let number2 = parseInt(number)
+                    let calc =  number1 / number2
+                    setResult(calc.toString())
+                    setNumber('')
+                }else if(reaper(result).operator === "x"){
+                    let number1 = parseInt(reaper(result).number)
+                    let number2 = parseInt(number)
+                    let calc =  number1 * number2
+                    setResult(calc.toString())
+                    setNumber('')
+                }else if(reaper(result).operator === "-"){
+                    let number1 = parseInt(reaper(result).number)
+                    let number2 = parseInt(number)
+                    let calc =  number1 - number2
+                    setResult(calc.toString())
+                    setNumber('')
+                }else if(reaper(result).operator === "+"){
+                    let number1 = parseInt(reaper(result).number)
+                    let number2 = parseInt(number)
+                    let calc =  number1 + number2
+                    setResult(calc.toString())
+                    setNumber('')
+                }
+            }
+        }else {
+            if(centinel(result) === true){
+                if(type === "operator"){
+                    setResult(prevState => {
+                        let example = reaper(prevState).number
+                        example = example + value
+                        return example
+                    })
+                }else{
+                    setNumber(prevState => {
+                        if (prevState === "0") {
+                            return value
+                        }else {
+                            const newNumber = prevState + value
+                            return newNumber
+                        }
+                    })
+                }
+            }else{
+                setResult(prevState => {
+                    if (prevState === "0") {
+                        return value
+                    }else {
+                        const newResult = prevState + value
+                        return newResult
+                    }
+                })
+            }
+        }
+    }
+
+    // ---- Centinel function
+    const centinel = (objetive) => {
+        const operators = ["/","x","-","+"]
+
+        let flag = false
+
+        operators.forEach(operator => {
+            if(objetive.includes(operator)){
+                flag = true
+            }
+        })
+
+        return flag
+    }
+
+    // ---- Reaper function
+    const reaper = (objetive) => {
+        const operators = ["/","x","-","+"]
+
+        let hook = ''
+        let finder = 0
+        let example = ''
+        let box = {}
+
+        operators.forEach(operator => {
+            if(objetive.includes(operator)){
+                finder = objetive.search(`/${operator}/`)
+                hook = operator
+                example = objetive.replace(operator, '')
+                
+                box = {
+                    position: finder,
+                    operator: hook,
+                    number: example,
+                }
+            }
+        })
+
+        return box
+    }
+
     return (
         <div className="y-calculator">
-            <div className="result-display"></div>
+            <div className="result-display">
+                <p style={number === "0" ? {color: "transparent" } : {color: "#00aaff"} }>{number}</p>
+                <p>{result}</p>
+            </div>
             <div className="num-pad">
-                <div className="button-01">C</div>
-                <div className="button-01">+/-</div>
-                <div className="button-01">%</div>
-                <div className="button-02">/</div>
-
-                <div className="button-01">1</div>
-                <div className="button-01">2</div>
-                <div className="button-01">3</div>
-                <div className="button-02">x</div>
-
-                <div className="button-01">4</div>
-                <div className="button-01">5</div>
-                <div className="button-01">6</div>
-                <div className="button-02">-</div>
-
-                <div className="button-01">7</div>
-                <div className="button-01">8</div>
-                <div className="button-01">9</div>
-                <div className="button-02">+</div>
-
-                <div className="button-01">0</div>
-                <div className="button-01">.</div>
-                <div className="button-01">delete</div>
-                <div className="button-02">=</div>
+                {numPadRender}
             </div>
         </div>
     )
---
+++ src/data/NumPad.js
@@ -0,0 +1,125 @@
+const NumPad = [
+    {
+        id: 1,
+        value: "C",
+        cssClass: "button-01",
+        type: "operator"
+    },
+    {
+        id: 2,
+        value: "+/-",
+        cssClass: "button-01",
+        type: "operator"
+    },
+    {
+        id: 3,
+        value: "%",
+        cssClass: "button-01",
+        type: "operator"
+    },
+    {
+        id: 4,
+        value: "/",
+        cssClass: "button-02",
+        type: "operator"
+    },
+    {
+        id: 5,
+        value: "1",
+        cssClass: "button-01",
+        type: "number"
+    },
+    {
+        id: 6,
+        value: "2",
+        cssClass: "button-01",
+        type: "number"
+    },
+    {
+        id: 7,
+        value: "3",
+        cssClass: "button-01",
+        type: "number"
+    },
+    {
+        id: 8,
+        value: "x",
+        cssClass: "button-02",
+        type: "operator"
+    },
+    {
+        id: 9,
+        value: "4",
+        cssClass: "button-01",
+        type: "number"
+    },
+    {
+        id: 10,
+        value: "5",
+        cssClass: "button-01",
+        type: "number"
+    },
+    {
+        id: 11,
+        value: "6",
+        cssClass: "button-01",
+        type: "number"
+    },
+    {
+        id: 12,
+        value: "-",
+        cssClass: "button-02",
+        type: "operator"
+    },
+    {
+        id: 13,
+        value: "7",
+        cssClass: "button-01",
+        type: "number"
+    },
+    {
+        id: 14,
+        value: "8",
+        cssClass: "button-01",
+        type: "number"
+    },
+    {
+        id: 15,
+        value: "9",
+        cssClass: "button-01",
+        type: "number"
+    },
+    {
+        id: 16,
+        value: "+",
+        cssClass: "button-02",
+        type: "operator"
+    },
+    {
+        id: 17,
+        value: "0",
+        cssClass: "button-01",
+        type: "number"
+    },
+    {
+        id: 18,
+        value: ".",
+        cssClass: "button-01",
+        type: "number"
+    },
+    {
+        id: 19,
+        value: "delete",
+        cssClass: "button-01",
+        type: "operator"
+    },
+    {
+        id: 20,
+        value: "=",
+        cssClass: "button-02",
+        type: "operator"
+    },
+
+]
+
+export default NumPad
