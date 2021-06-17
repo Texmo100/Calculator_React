1d4e07c9ead889e6c3598da18054c6935e345ab2
Texmo100 <isaac.ir@hotmail.com>
2021-06-16 17:01:50 -0700

    y-Calculator alpha version 2.0

--- src/components/Ycalculator.js
+++ src/components/Ycalculator.js
@@ -2,70 +2,111 @@ import React, { useState } from 'react'
 import NumPad from '../data/NumPad'
 
 const Ycalculator = () => {
+    // ---- State using useState
     const [result, setResult] = useState("0")
     const [number, setNumber] = useState("0")
 
+    // ---- Render al numpad (numbers and operators)
     const numPadRender = NumPad.map(item => <button key={item.id} className={item.cssClass} onClick={() => buttonHandle(item)}>{item.value}</button>)
 
+    // ---- Handle button to control all button's behavior
     const buttonHandle = (item) => {
         const { type, value } = item
 
         if ((type === "operator") && (value === "C")) {
             setResult("0")
             setNumber("0")
-        }else if ((type === "operator") && (value === "=")){
-            if(centinel(result) === false){
-                setResult(result)
+        }else if((type === "operator") && (value === "delete")){
+            if((centinel(result) === true) && (number === "0")){
+                if(result === "0"){
+                    setResult("0")
+                }else{
+                    let extractor = result.slice(0, -1)
+                    if(extractor.length === 0){
+                        setResult("0")
+                    }else{
+                        setResult(extractor)
+                    }
+                }
+            }else if(centinel(result) === true){
+                if(result === "0"){
+                    setNumber("0")
+                }else{
+                    let extractor = number.slice(0, -1)
+                    if(extractor.length === 0){
+                        setNumber("0")
+                    }else{
+                        setNumber(extractor)
+                    }
+                }
             }else{
-                if(reaper(result).operator === "/"){
-                    let number1 = parseInt(reaper(result).number)
-                    let number2 = parseInt(number)
-                    let calc =  number1 / number2
-                    setResult(calc.toString())
-                    setNumber('')
-                }else if(reaper(result).operator === "x"){
-                    let number1 = parseInt(reaper(result).number)
-                    let number2 = parseInt(number)
-                    let calc =  number1 * number2
-                    setResult(calc.toString())
-                    setNumber('')
-                }else if(reaper(result).operator === "-"){
-                    let number1 = parseInt(reaper(result).number)
-                    let number2 = parseInt(number)
-                    let calc =  number1 - number2
-                    setResult(calc.toString())
-                    setNumber('')
-                }else if(reaper(result).operator === "+"){
-                    let number1 = parseInt(reaper(result).number)
-                    let number2 = parseInt(number)
-                    let calc =  number1 + number2
-                    setResult(calc.toString())
-                    setNumber('')
+                if(result === "0"){
+                    setResult("0")
+                }else{
+                    let extractor = result.slice(0, -1)
+                    if(extractor.length === 0){
+                        setResult("0")
+                    }else{
+                        setResult(extractor)
+                    }
                 }
             }
-        }else {
-            if(centinel(result) === true){
-                if(type === "operator"){
+        }else if ((type === "operator") && (value === "=")) {
+            if (centinel(result) === false) {
+                setResult(result)
+            } else {
+                let number1 = 0
+                let number2 = 0
+
+                if (isDecimal(result) === true) {
+                    number1 = parseFloat(reaper(result).number)
+                    number2 = parseFloat(number)
+                } else {
+                    number1 = parseInt(reaper(result).number)
+                    number2 = parseInt(number)
+                }
+
+                if (reaper(result).operator === "/") {
+                    let calc = number1 / number2
+                    setResult(calc.toString())
+                    setNumber('')
+                } else if (reaper(result).operator === "x") {
+                    let calc = number1 * number2
+                    setResult(calc.toString())
+                    setNumber('0')
+                } else if (reaper(result).operator === "-") {
+                    let calc = number1 - number2
+                    setResult(calc.toString())
+                    setNumber('0')
+                } else if (reaper(result).operator === "+") {
+                    let calc = number1 + number2
+                    setResult(calc.toString())
+                    setNumber('0')
+                }
+            }
+        } else {
+            if (centinel(result) === true) {
+                if (type === "operator") {
                     setResult(prevState => {
                         let example = reaper(prevState).number
                         example = example + value
                         return example
                     })
-                }else{
+                } else {
                     setNumber(prevState => {
                         if (prevState === "0") {
                             return value
-                        }else {
+                        } else {
                             const newNumber = prevState + value
                             return newNumber
                         }
                     })
                 }
-            }else{
+            } else {
                 setResult(prevState => {
                     if (prevState === "0") {
                         return value
-                    }else {
+                    } else {
                         const newResult = prevState + value
                         return newResult
                     }
@@ -74,14 +115,14 @@ const Ycalculator = () => {
         }
     }
 
-    // ---- Centinel function
-    const centinel = (objetive) => {
-        const operators = ["/","x","-","+"]
+    // ---- Centinel function (to verify if there's an operator in calc)
+    const centinel = objetive => {
+        const operators = ["/", "x", "-", "+"]
 
         let flag = false
 
         operators.forEach(operator => {
-            if(objetive.includes(operator)){
+            if (objetive.includes(operator)) {
                 flag = true
             }
         })
@@ -89,9 +130,9 @@ const Ycalculator = () => {
         return flag
     }
 
-    // ---- Reaper function
-    const reaper = (objetive) => {
-        const operators = ["/","x","-","+"]
+    // ---- Reaper function (locate and identify the number, operator and position)
+    const reaper = objetive => {
+        const operators = ["/", "x", "-", "+"]
 
         let hook = ''
         let finder = 0
@@ -99,11 +140,11 @@ const Ycalculator = () => {
         let box = {}
 
         operators.forEach(operator => {
-            if(objetive.includes(operator)){
+            if (objetive.includes(operator)) {
                 finder = objetive.search(`/${operator}/`)
                 hook = operator
                 example = objetive.replace(operator, '')
-                
+
                 box = {
                     position: finder,
                     operator: hook,
@@ -115,10 +156,18 @@ const Ycalculator = () => {
         return box
     }
 
+    // ---- Verify if the objetive is decimal number
+    const isDecimal = objetive => {
+        if (objetive.includes('.')) {
+            return true
+        }
+        return false
+    }
+
     return (
         <div className="y-calculator">
             <div className="result-display">
-                <p style={number === "0" ? {color: "transparent" } : {color: "#00aaff"} }>{number}</p>
+                <p style={number === "0" ? { color: "transparent" } : { color: "#00aaff" }}>{number}</p>
                 <p>{result}</p>
             </div>
             <div className="num-pad">
